816e960b6e69ea13673e80ae79de5ddbb70c9b3c
Caleb Kira <calebnoborukira@gmail.com>
2025-12-02 14:11:09 -0800

    added vite and lit and am beginning to make the custom components. Need to make one for the navigation bar and the writing card (describing the category and the specific files associated with it)

---
+++ packages/proto/src/writing-card.ts
@@ -0,0 +1,12 @@
+import { html, css, LitElement } from "lit";
+import { property } from "lit/decorators.js";
+
+export class WritingCard extends LitElement {
+  override render() {
+    return html`
+      <!-- paste your template here -->
+    `;
+  }
+
+  static styles = css``;
+}
